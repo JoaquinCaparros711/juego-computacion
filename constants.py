d62f777bc2ccb22791659551ec821dad5648baed
HerreroT <t.herrero@alumno.um.edu.ar>
2024-08-13 13:41:08 -0300

    menu

---
+++ constants.py
@@ -0,0 +1,7 @@
+import os
+def print_menu():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"{'-'*40}")
+    print(f"{' MENÚ PRINCIPAL ':-^40}")
+    print(f"{'-'*40}")
+    print(f"{'-'*40}")
