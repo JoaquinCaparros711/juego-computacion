3dba4212a21ab47525d0e8b6c696fb47ddff79b2
Joaquín Caparros <105978542+JoaquinCaparros711@users.noreply.github.com>
2024-08-13 14:16:09 -0300

    Trabaje en la clase Character

--- classes/character.py
+++ classes/character.py
@@ -10,3 +10,6 @@ class Character:
         self.__strength = strength
         self.__defense = defense
         self.__level = level
+    
+    def __str__(self):
+        return f"{self.__name} - Salud: {self.__health}, Fuerza: {self.__strength}, Defensa: {self.__defense}, Nivel: {self.__level}"
