19b2130162f6d90aff255eb3adab7cc0134c75d7
Joaquín Caparros <105978542+JoaquinCaparros711@users.noreply.github.com>
2024-08-13 13:46:39 -0300

    Cree la clase Character

--- classes/character.py
+++ classes/character.py
@@ -1,1 +1,12 @@
-print("PRUEBA DE GIt")
+
+
+
+
+class Character:
+
+    def __init__(self, name, health, strength, defense, level = 1):
+        self.__name = name
+        self.__health = health
+        self.__strength = strength
+        self.__defense = defense
+        self.__level = level
